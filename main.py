5b6de78255e6ace287ed564d3593d27fede0dcdc
Fiante <80510301+Fiante@users.noreply.github.com>
2021-12-01 20:25:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import platform
+
+# İşletim Sistemini Öğrenme - Learning The Operating System
+print(platform.system())
+
+# İşlemci Bilgisini Çğrenme - Learning Processor Information
+print(platform.processor())
+
+# Nesil Bilgisi Öğrenme     - learning Generation Knowledge
+print(platform.release())
+
+# Versiyon Öğrenme          - learning Version
+print(platform.python_version())
+
+# Mimari Bilgisini Öğrenme  - Learning Architectural Knowledge
+print(platform.architecture())
+
+# Makine Bilgisini Öğrenme  - Learning Machine  Knowledge
+print(platform.machine())
+
+# Bağlantı Adını Öğrenme    - Learning Connection Name
+print(platform.node())
+
+# Compiler Bilgisi Öğrenme  - Learning Compiler İnfo
+print(platform.python_compiler())
+
+# Python Versiyon Öğrenme/v - Learning Py Version
+print(platform.python_branch())
+
+# Py İmplementasyonu Öğrenme- Learning Py İmplementation
+print(platform.python_implementation())
